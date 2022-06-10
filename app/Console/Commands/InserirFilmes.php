5b7741c55369b05ba5d25ac2bae17667d2cd9375
Marcelo Zapatta <marcelozapatta0@gmail.com>
2022-06-10 14:29:35 -0300

    fix: DS Store

--- app/Console/Commands/InserirFilmes.php
+++ app/Console/Commands/InserirFilmes.php
@@ -34,51 +34,56 @@ class InserirFilmes extends Command
     {
         $filmes = Storage::disk('public_folder')->allFiles('filmes');
 
-        foreach ($filmes as $filme) {
-            $nomeArquivo = pathinfo($filme, PATHINFO_FILENAME);
-            $nomeArquivo = explode('||', $nomeArquivo);
-
-            $rankingFilme = $nomeArquivo[0];
-            $nomeFilme = $nomeArquivo[1];
-            $anoFilme = $nomeArquivo[2];
-            $diretorFilme = $nomeArquivo[3];
-            $imdbLink = urldecode($nomeArquivo[4]);
-
-            $diretor = Diretor::updateOrCreate([
-                'nome' => $diretorFilme
-            ]);
-
-            $uuid = Str::uuid();
-
-            $filename = $uuid . '.webp';
-
-            Storage::write('filmes/' . $filename, Image::make(public_path($filme))
-                ->resize(1920, null, function ($constraint) {
-                    $constraint->aspectRatio();
-                    $constraint->upsize();
-                })
-                ->resize(null, 900, function ($constraint) {
-                    $constraint->aspectRatio();
-                    $constraint->upsize();
-                })
-                ->encode('webp', 90)
-                ->response('webp')->content());
-
-            $filenameResized = $uuid . '-250-350.webp';
-
-            Storage::write('filmes/' . $filenameResized, Image::make(public_path($filme))
-                ->fit(250, 350)
-                ->encode('webp', 90)
-                ->response('webp')->content());
-
-            Filme::create([
-                'nome' => $nomeFilme,
-                'diretor_id' => $diretor->id,
-                'rank' => $rankingFilme,
-                'foto' => 'filmes/' . $filename,
-                'imdb_link' => $imdbLink,
-                'ano' => $anoFilme
-            ]);
+        try {
+            //code...
+            foreach ($filmes as $filme) {
+                $nomeArquivo = pathinfo($filme, PATHINFO_FILENAME);
+                $nomeArquivo = explode('||', $nomeArquivo);
+                dd($filme);
+                $rankingFilme = $nomeArquivo[0];
+                $nomeFilme = $nomeArquivo[1];
+                $anoFilme = $nomeArquivo[2];
+                $diretorFilme = $nomeArquivo[3];
+                $imdbLink = urldecode($nomeArquivo[4]);
+    
+                $diretor = Diretor::updateOrCreate([
+                    'nome' => $diretorFilme
+                ]);
+    
+                $uuid = Str::uuid();
+    
+                $filename = $uuid . '.webp';
+    
+                Storage::write('filmes/' . $filename, Image::make(public_path($filme))
+                    ->resize(1920, null, function ($constraint) {
+                        $constraint->aspectRatio();
+                        $constraint->upsize();
+                    })
+                    ->resize(null, 900, function ($constraint) {
+                        $constraint->aspectRatio();
+                        $constraint->upsize();
+                    })
+                    ->encode('webp', 90)
+                    ->response('webp')->content());
+    
+                $filenameResized = $uuid . '-250-350.webp';
+    
+                Storage::write('filmes/' . $filenameResized, Image::make(public_path($filme))
+                    ->fit(250, 350)
+                    ->encode('webp', 90)
+                    ->response('webp')->content());
+    
+                Filme::create([
+                    'nome' => $nomeFilme,
+                    'diretor_id' => $diretor->id,
+                    'rank' => $rankingFilme,
+                    'foto' => 'filmes/' . $filename,
+                    'imdb_link' => $imdbLink,
+                    'ano' => $anoFilme
+                ]);
+            }
+        } catch (\Throwable $th) {
+            dd($th->getMessage(), $th->getLine());
         }
 
         return 0;
