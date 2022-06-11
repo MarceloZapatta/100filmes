315c9d2d44f187a9b398201f24625c81acdf6ca4
Marcelo Zapatta <marcelozapatta0@gmail.com>
2022-06-11 16:24:45 -0300

    fix: register link

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -42,12 +42,12 @@
 
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900 mr-2" href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
                     </a>
                 @endif
 
-                <a href="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('register') }}">
+                <a class="underline text-sm text-gray-600 hover:text-gray-900 mr-2" href="{{ route('register') }}">
                     Registrar
                 </a>
 
